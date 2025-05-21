5c047347dec7956caf369fc4408fc12393190f1e
filzaelang <filzaelang@gmail.com>
2025-05-21 15:38:05 +0700

    finish fe menu

--- views/src/components/MenuItem.jsx
+++ views/src/components/MenuItem.jsx
@@ -1,17 +1,41 @@
-import React from "react";
-import { FaFolder } from "react-icons/fa";
+import React, { useState } from "react";
 import { Link } from "@inertiajs/react";
+import { FaChevronRight, FaChevronDown, FaFolder } from "react-icons/fa";
 
-function MenuItem({ name, uri }) {
+function MenuItem({ menu, level = 0 }) {
+  const [open, setOpen] = useState(false);
+  const hasChildren = menu.children && menu.children.length > 0;
+
   return (
-    <>
-      <div className="menu-link">
-        <FaFolder className="icon-link" />
-        <Link className="text-link" href={uri}>
-          {name}
-        </Link>
+    <div className={`pl-${level * 4}`}>
+      <div
+        className="menu-link cursor-pointer flex items-center gap-2"
+        onClick={() => hasChildren && setOpen(!open)}
+      >
+        {menu.icon || <FaFolder className="icon-link" />}
+        {menu.uri ? (
+          <Link className="text-link" href={menu.uri}>
+            {menu.name}
+          </Link>
+        ) : (
+          <span className="text-link">{menu.name}</span>
+        )}
+        {hasChildren &&
+          (open ? (
+            <FaChevronDown size={12} className="text-link" />
+          ) : (
+            <FaChevronRight size={12} className="text-link" />
+          ))}
       </div>
-    </>
+
+      {hasChildren && open && (
+        <div className="ml-4">
+          {menu.children.map((child, idx) => (
+            <MenuItem key={idx} menu={child} level={level + 1} />
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
 
--- views/src/components/MenuSidebar.jsx
+++ views/src/components/MenuSidebar.jsx
@@ -1,59 +1,86 @@
-import { Link } from "@inertiajs/react";
 import { FaHome } from "react-icons/fa";
 import { FaUser } from "react-icons/fa6";
 import { FaGears } from "react-icons/fa6";
 import { FaStar } from "react-icons/fa6";
 import { IoDocuments } from "react-icons/io5";
 import MenuItem from "./MenuItem";
+import { FaBuilding } from "react-icons/fa6";
+import { FaBriefcase } from "react-icons/fa";
+import { FaFolderOpen } from "react-icons/fa6";
+import { HiMiniUserGroup } from "react-icons/hi2";
+
+const menus = [
+  {
+    name: "Dashboard",
+    uri: "/dashboard",
+    icon: <FaHome className="icon-link" />,
+  },
+  {
+    name: "Profile",
+    uri: "/profile",
+    icon: <FaUser className="icon-link" />,
+  },
+  {
+    name: "Setup",
+    icon: <FaGears className="icon-link" />,
+    children: [
+      {
+        name: "Profile Authority",
+        uri: "/setup/profile-authority",
+        icon: <HiMiniUserGroup className="icon-link" />,
+      },
+      {
+        name: "Folder Management",
+        uri: "/setup/folder-management",
+        icon: <FaFolderOpen className="icon-link" />,
+      },
+      {
+        name: "Department Management",
+        uri: "/setup/department-management",
+        icon: <FaBuilding className="icon-link" />,
+      },
+      {
+        name: "Check files",
+        uri: "/setup/check-files",
+        icon: <FaBriefcase className="icon-link" />,
+      },
+    ],
+  },
+  {
+    name: "Favorites",
+    uri: "/favorites",
+    icon: <FaStar className="icon-link" />,
+  },
+  {
+    name: "All Files",
+    uri: "/allfiles",
+    icon: <IoDocuments className="icon-link" />,
+  },
+  {
+    name: "Dummy",
+    children: [
+      {
+        name: "User Setup",
+        uri: "/setup/user",
+      },
+      {
+        name: "Roles",
+        uri: "/setup/roles",
+        children: [
+          { name: "Admin", uri: "/setup/roles/admin" },
+          { name: "User", uri: "/setup/roles/user" },
+        ],
+      },
+    ],
+  },
+];
 
 function MenuSidebar() {
   return (
     <>
-      <div className="flex flex-col">
-        <div className="menu-link">
-          <FaHome className="icon-link" />
-          <Link className="text-link" href="/dashboard">
-            Dashboard
-          </Link>
-        </div>
-        <div className="menu-link">
-          <FaUser className="icon-link" />
-          <Link className="text-link" href="/profile">
-            Profile
-          </Link>
-          <span className="tooltip">Profile</span>
-        </div>
-        <div className="menu-link">
-          <FaGears className="icon-link" />
-          <Link className="text-link" href="/setup">
-            Setup
-          </Link>
-        </div>
-        <div className="menu-link">
-          <FaStar className="icon-link" />
-          <Link className="text-link" href="/favorites">
-            Favorites
-          </Link>
-        </div>
-        <div className="menu-link">
-          <IoDocuments className="icon-link" />
-          <Link className="text-link" href="/allfiles">
-            All Files
-          </Link>
-        </div>
-        <MenuItem
-          name={"Dummy Quality Control"}
-          uri={"/dummy-quality-control"}
-        />
-        <MenuItem
-          name={"Dummy Standard Procedure"}
-          uri={"/dummy-standard-procedure"}
-        />
-        <MenuItem
-          name={"Dummy Standard Instruction"}
-          uri={"/dummy-standard-instruction"}
-        />
-      </div>
+      {menus.map((menu, index) => (
+        <MenuItem key={index} menu={menu} />
+      ))}
     </>
   );
 }
